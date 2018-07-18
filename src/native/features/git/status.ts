f4d52a38211b3ff5fb9aa967bdca9edd2ba5ee0c
Phodal HUANG <h@phodal.com>
2018-07-18 15:48:42 +0800

    [T] update git status

--- src/native/features/git/status.ts
+++ src/native/features/git/status.ts
@@ -1,7 +1,7 @@
 import spawnGit from './spawn-git';
 
 export default function gitStatus(path: string) {
-  spawnGit('status', {cwd: path}, function (err: any, stdout: any, stdrr: any) {
+  spawnGit('status --porcelain', {cwd: path}, function (err: any, stdout: any, stdrr: any) {
     if (err) {
       return;
     }
