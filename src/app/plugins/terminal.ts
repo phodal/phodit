d85126967d1208de58337106c69b1b060f0ab22a
Phodal HUANG <h@phodal.com>
2018-07-26 11:16:39 +0800

    [spike] termnial: default hidden

--- src/app/plugins/terminal.ts
+++ src/app/plugins/terminal.ts
@@ -2,24 +2,26 @@ let os = require('os');
 let pty = require('node-pty');
 let Terminal = require('xterm').Terminal;
 
+function createTerminal(path?: string) {
 // Initialize node-pty with an appropriate shell
-const shell = process.env[os.platform() === 'win32' ? 'COMSPEC' : 'SHELL'];
-const ptyProcess = pty.spawn(shell, [], {
-  name: 'xterm-color',
-  cols: 80,
-  rows: 30,
-  cwd: process.cwd(),
-  env: process.env
-});
+  const shell = process.env[os.platform() === 'win32' ? 'COMSPEC' : 'SHELL'];
+  const ptyProcess = pty.spawn(shell, [], {
+    name: 'xterm-color',
+    cols: 80,
+    rows: 30,
+    cwd: path || process.cwd(),
+    env: process.env
+  });
 
 // Initialize xterm.js and attach it to the DOM
-const xterm = new Terminal();
-xterm.open(document.getElementById('terminal'));
+  const xterm = new Terminal();
+  xterm.open(document.getElementById('terminal'));
 
 // Setup communication between xterm.js and node-pty
-xterm.on('data', (data: any) => {
-  ptyProcess.write(data);
-});
-ptyProcess.on('data', function (data: any) {
-  xterm.write(data);
-});
+  xterm.on('data', (data: any) => {
+    ptyProcess.write(data);
+  });
+  ptyProcess.on('data', function (data: any) {
+    xterm.write(data);
+  });
+}
