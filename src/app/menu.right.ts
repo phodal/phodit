286e14dd4da243e83cf1ec0f4bf47d292ddf5c76
Phodal HUANG <h@phodal.com>
2018-07-26 16:29:49 +0800

    [T] thinking in left menu

--- src/app/menu.right.ts
+++ src/app/menu.right.ts
@@ -6,42 +6,46 @@ const globalStore = {
   eventTarget: {}
 };
 
-menu.append(new MenuItem({
-  label: "Google", click() {
-    let text = (globalStore.eventTarget as any).innerText;
-    require('electron').shell.openExternal(`https://www.google.com/search?q=${text}`);
-  },
-}));
+function createEditorMenu() {
+  menu.append(new MenuItem({
+    label: "Google", click() {
+      let text = (globalStore.eventTarget as any).innerText;
+      require('electron').shell.openExternal(`https://www.google.com/search?q=${text}`);
+    },
+  }));
 
-menu.append(new MenuItem({
-  label: "Baidu", click() {
-    let text = (globalStore.eventTarget as any).innerText;
-    require('electron').shell.openExternal(`https://www.baidu.com/s?wd=${text}`);
-  },
-}));
+  menu.append(new MenuItem({
+    label: "Baidu", click() {
+      let text = (globalStore.eventTarget as any).innerText;
+      require('electron').shell.openExternal(`https://www.baidu.com/s?wd=${text}`);
+    },
+  }));
 
-menu.append(new MenuItem({
-  label: "GitHub", click() {
-    let text = (globalStore.eventTarget as any).innerText;
-    require('electron').shell.openExternal(`https://github.com/search?q=${text}&ref=opensearch`);
-  },
-}));
+  menu.append(new MenuItem({
+    label: "GitHub", click() {
+      let text = (globalStore.eventTarget as any).innerText;
+      require('electron').shell.openExternal(`https://github.com/search?q=${text}&ref=opensearch`);
+    },
+  }));
 
-menu.append(new MenuItem({
-  label: "Wiki", click() {
-    let text = (globalStore.eventTarget as any).innerText;
-    require('electron').shell.openExternal(`https://zh.wikipedia.org/wiki/Special:Search?search=${text}`);
-  },
-}));
+  menu.append(new MenuItem({
+    label: "Wiki", click() {
+      let text = (globalStore.eventTarget as any).innerText;
+      require('electron').shell.openExternal(`https://zh.wikipedia.org/wiki/Special:Search?search=${text}`);
+    },
+  }));
 
-menu.append(new MenuItem({
-  label: "Clipboard", click() {
+  menu.append(new MenuItem({
+    label: "Clipboard", click() {
 
-  },
-}));
+    },
+  }));
+
+}
 
 window.addEventListener("contextmenu", (event: MouseEvent) => {
   event.preventDefault();
   globalStore.eventTarget = event.target;
+  createEditorMenu();
   menu.popup({window: remote.getCurrentWindow()});
 }, false);
