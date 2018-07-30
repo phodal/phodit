bc1d9a57d8af10956099ee466ae50baeb69f9eaa
Phodal HUANG <h@phodal.com>
2018-07-30 14:45:06 +0800

    [spike] pandoc: remove unusd vars

--- src/native/features/pandoc/word.ts
+++ src/native/features/pandoc/word.ts
@@ -1,7 +1,7 @@
 import spawnPandoc from "./spawn-pandoc";
 
 export default function html(path: string) {
-  spawnPandoc(`-s ${path} -t html5 -o index.html`, {cwd: path}, function(err: any, stdout: any, stdrr: any) {
+  spawnPandoc(`-s ${path} -t html5 -o index.html`, {}, function(err: any, stdout: any, stdrr: any) {
     if (err) {
       return;
     }
