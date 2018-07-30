2d241b962752c88b90ecfbdc816e2ba72db93f1c
Phodal HUANG <h@phodal.com>
2018-07-30 17:22:07 +0800

    [T] update content

--- src/native/features/pandoc/word.ts
+++ src/native/features/pandoc/word.ts
@@ -1,14 +1,14 @@
 import spawnPandoc from "./spawn-pandoc";
 import {shell} from "electron";
 
-export default function html(path: string) {
+export default function word(path: string) {
   let newPath = path.replace(/\.md/, '.docx');
   spawnPandoc(`-s ${path} -o ${newPath}`, {}, function(err: any, stdout: any, stdrr: any) {
     if (err) {
       return;
     }
 
-    shell.openExternal(newPath);
+    shell.openItem(newPath);
     return stdout.trim();
   });
 
