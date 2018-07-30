e6c29a9b536035f9965a2b2efd35182dcfa9cd4f
Phodal HUANG <h@phodal.com>
2018-07-30 14:49:28 +0800

    [T] create docx in path

--- src/native/features/pandoc/word.ts
+++ src/native/features/pandoc/word.ts
@@ -1,7 +1,8 @@
 import spawnPandoc from "./spawn-pandoc";
 
 export default function html(path: string) {
-  spawnPandoc(`-s ${path} -o export.docx`, {}, function(err: any, stdout: any, stdrr: any) {
+  let newPath = path.replace(/\.md/, '.docx');
+  spawnPandoc(`-s ${path} -o ${newPath}`, {}, function(err: any, stdout: any, stdrr: any) {
     if (err) {
       return;
     }
