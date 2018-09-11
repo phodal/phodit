93383cdc27d0cb6c4c14b3696929e7feb7a6be41
Phodal HUANG <h@phodal.com>
2018-09-12 07:41:25 +0800

    [T] pdf: use html as router

--- src/native/features/pandoc/pdf.ts
+++ src/native/features/pandoc/pdf.ts
@@ -1,16 +1,25 @@
 import spawnPandoc from "./spawn-pandoc";
 import {shell} from "electron";
+import * as fs from "fs";
 
 export default function pdf(path: string) {
-  let newPath = path.replace(/\.md/, '.pdf');
-  spawnPandoc(`-s ${path} -o ${newPath}`, {}, function(err: any, stdout: any, stdrr: any) {
+  let htmlPath = path.replace(/\.md/, '.html');
+  let pdfPath = path.replace(/\.md/, '.pdf');
+  spawnPandoc(`-s ${path} -o ${htmlPath}`, {}, function(err: any, stdout: any, stdrr: any) {
     if (err) {
       console.error(err);
       return;
     }
 
-    shell.openItem(newPath);
-    return stdout.trim();
-  });
+    spawnPandoc(`-s ${htmlPath} -o ${pdfPath}`, {}, function (err: any, stdout: any, stdrr: any) {
+      if (err) {
+        console.error(err);
+        return;
+      }
 
+      fs.unlink(htmlPath, () => {});
+      shell.openItem(pdfPath);
+      return stdout.trim();
+    });
+  });
 }
