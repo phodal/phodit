38b1c51da76050e5c31230844b2440040a357109
Phodal HUANG <h@phodal.com>
2019-01-16 07:29:47 +0800

    refactor: refactor UI

--- src/app/renderer.ts
+++ src/app/renderer.ts
@@ -8,8 +8,6 @@ import {getCodeMirrorMode} from "./utils/file.utils";
 import {EventConstants} from "../common/constants/event.constants";
 
 // require("devtron").install();
-
-
 declare global {
   interface Window {
     simplemde: any
@@ -19,225 +17,227 @@ declare global {
 const {ipcRenderer} = require("electron");
 const swal = require("sweetalert");
 
-let state = {
-  isShowTerminal: false,
-  hasCreateTerminal: false,
-  currentFile: '',
-  isCurrentFileTemp: false,
-  isOneFile: false,
-  isPath: false
+const ClientUI = function () {
+
 };
 
-const simplemde = new (window as any).SimpleMDE({
-  spellChecker: false,
-  autosave: {
-    enabled: true,
-    uniqueId: "phodit",
-    delay: 1000,
-  },
-  promptTexts: {
-    link: 'link',
-    image: 'image'
-  },
-  autoDownloadFontAwesome: false,
-  renderingConfig: {
-    codeSyntaxHighlighting: true
-  },
-  element: document.getElementById("input-section"),
-});
+ClientUI.prototype.init = function () {
+  this.state = {
+    isShowTerminal: false,
+    hasCreateTerminal: false,
+    currentFile: '',
+    isCurrentFileTemp: false,
+    isOneFile: false,
+    isPath: false
+  };
 
-window.simplemde = simplemde;
+  (window as any).simplemde = this.simplemde = new (window as any).SimpleMDE({
+    spellChecker: false,
+    autosave: {
+      enabled: true,
+      uniqueId: "phodit",
+      delay: 1000,
+    },
+    promptTexts: {
+      link: 'link',
+      image: 'image'
+    },
+    autoDownloadFontAwesome: false,
+    renderingConfig: {
+      codeSyntaxHighlighting: true
+    },
+    element: document.getElementById("input-section"),
+  });
 
 // @ts-ignore
-const clipboard = new ClipboardJS('.wechat-button');
+  const clipboard = new ClipboardJS('.wechat-button');
 
-clipboard.on('success', function (event: any) {
-  swal({
-    title: "Copy Success", icon: "success", dangerMode: true,
-    buttons: {
-      confirm: {text: "OK"}
-    }
-  })
-  event.clearSelection();
-});
+  clipboard.on('success', function (event: any) {
+    swal({
+      title: "Copy Success", icon: "success", dangerMode: true,
+      buttons: {
+        confirm: {text: "OK"}
+      }
+    });
+    event.clearSelection();
+  });
+};
 
 function updatePos(currentFile: string) {
   let lastPos = localStorage.getItem("line_" + currentFile);
   if (lastPos) {
     let parsedPos = JSON.parse(lastPos);
-    simplemde.codemirror.setCursor(parsedPos.line, parsedPos.ch);
+    this.simplemde.codemirror.setCursor(parsedPos.line, parsedPos.ch);
   }
 }
 
-// 打开帮助
-window.document.addEventListener(EventConstants.CLIENT.OPEN_GUIDE, (data) => {
-  ipcRenderer.send(EventConstants.PHODIT.OPEN_GUIDE, simplemde.value());
-});
+ClientUI.prototype.bindEvent = function () {
+  // 打开帮助
+  window.document.addEventListener(EventConstants.CLIENT.OPEN_GUIDE, (data) => {
+    ipcRenderer.send(EventConstants.PHODIT.OPEN_GUIDE, this.simplemde.value());
+  });
 
-// 全屏
-window.document.addEventListener(EventConstants.CLIENT.FULL_SCREEN, (data) => {
-  document.getElementById('input').classList.add('full-screen');
-  ipcRenderer.send(EventConstants.PHODIT.FULL_SCREEN);
-});
+  // 全屏
+  window.document.addEventListener(EventConstants.CLIENT.FULL_SCREEN, (data) => {
+    document.getElementById('input').classList.add('full-screen');
+    ipcRenderer.send(EventConstants.PHODIT.FULL_SCREEN);
+  });
 
-// 取消全屏
-window.document.addEventListener(EventConstants.CLIENT.UN_FULL_SCREEN, (data) => {
-  document.getElementById('input').classList.remove('full-screen');
-  ipcRenderer.send(EventConstants.PHODIT.UN_FULL_SCREEN);
-});
+  // 取消全屏
+  window.document.addEventListener(EventConstants.CLIENT.UN_FULL_SCREEN, (data) => {
+    document.getElementById('input').classList.remove('full-screen');
+    ipcRenderer.send(EventConstants.PHODIT.UN_FULL_SCREEN);
+  });
 
-// Terminal
-window.document.addEventListener(EventConstants.CLIENT.SHOW_TERMINAL, () => {
-  if (!state.hasCreateTerminal) {
-    createTerminal(removeLastDirectoryPartOf(state.currentFile));
-    state.hasCreateTerminal = true;
-  }
+  // Terminal show
+  window.document.addEventListener(EventConstants.CLIENT.SHOW_TERMINAL, () => {
+    if (!this.state.hasCreateTerminal) {
+      createTerminal(removeLastDirectoryPartOf(this.state.currentFile));
+      this.state.hasCreateTerminal = true;
+    }
 
-  state.isShowTerminal = !state.isShowTerminal;
-  if (state.isShowTerminal) {
-    document.getElementById('terminal-section').setAttribute('style', "display: block;");
-  } else {
+    this.state.isShowTerminal = !this.state.isShowTerminal;
+    if (this.state.isShowTerminal) {
+      document.getElementById('terminal-section').setAttribute('style', "display: block;");
+    } else {
+      document.getElementById('terminal-section').setAttribute('style', "display: none;");
+    }
+  });
+
+  // Terminal hidden
+  window.document.addEventListener(EventConstants.CLIENT.HIDDEN_TERMINAL, () => {
     document.getElementById('terminal-section').setAttribute('style', "display: none;");
-  }
-});
-
-// Terminal
-window.document.addEventListener(EventConstants.CLIENT.HIDDEN_TERMINAL, () => {
-  document.getElementById('terminal-section').setAttribute('style', "display: none;");
-});
-
-// ShowSlides
-window.document.addEventListener(EventConstants.CLIENT.SHOW_SLIDES, () => {
-  if (!state.currentFile) {
-    return;
-  }
-  ipcRenderer.send(EventConstants.PHODIT.SHOW_SLIDES, {
-    isTempFile: state.isCurrentFileTemp,
-    file: state.currentFile,
-    data: simplemde.value(),
   });
-});
 
-// 隐藏 SIDE
-window.document.addEventListener(EventConstants.CLIENT.HIDDEN_SIDE, () => {
-  document.getElementById('tree-view').setAttribute('style', "display: none;");
-  document.querySelector(".wechat-button").setAttribute("data-clipboard-target", ".editor-preview-side")
-});
-
-// 展示 SIDE
-window.document.addEventListener(EventConstants.CLIENT.SHOW_SIDE, () => {
-  document.querySelector(".wechat-button").removeAttribute("data-clipboard-target")
-  if (state.isPath) {
-    document.getElementById('tree-view').setAttribute('style', "display: block;");
-  }
-});
-
-// 发起获取自动完成请求
-window.document.addEventListener(EventConstants.CLIENT.GET_SUGGEST, (data: any) => {
-  ipcRenderer.send(EventConstants.PHODIT.GET_SUGGEST, data.detail);
-});
-
-// 返回获取自动完成请求
-ipcRenderer.on(EventConstants.PHODIT.SUGGEST_SEND, (event: any, arg: any) => {
-  createEvent(EventConstants.PHODIT.SUGGEST_TO_EDITOR, arg);
-});
-
-// 打开文件
-ipcRenderer.on(EventConstants.PHODIT.OPEN_ONE_FILE, (event: any, arg: IFileOpen) => {
-  state.currentFile = arg.file;
-  state.isOneFile = true;
-  simplemde.codemirror.setOption("mode", getCodeMirrorMode(state.currentFile));
-  state.isCurrentFileTemp = arg.isTempFile;
-  simplemde.value(arg.data);
-  updatePos(arg.file);
-
-  localStorage.setItem('currentFile', arg.file);
-});
-
-// 保存文件
-ipcRenderer.on(EventConstants.CLIENT.SAVE_FILE, () => {
-  ipcRenderer.send(EventConstants.PHODIT.SAVE_FILE, {
-    isTempFile: state.isCurrentFileTemp,
-    data: simplemde.value(),
-  });
-});
-
-// 打开某一目录
-ipcRenderer.on(EventConstants.PHODIT.OPEN_PATH, (event: any, arg: any) => {
-  state.isPath = true;
-  document.getElementById('tree-view').setAttribute('style', "display: block");
-
-  createEvent("phodit.tree.open", {
-    path: arg.path,
-    tree: arg.tree
-  });
-});
-
-// 改变临时文件的状态
-ipcRenderer.on(EventConstants.TEMP_FILE_STATUS, (event: any, arg: any) => {
-  state.isCurrentFileTemp = arg.isTempFile;
-});
-
-// 加载完成
-ipcRenderer.on('phodit.lifecycle.load', (event: any, arg: any) => {
-  // if (!state.currentFile) {
-  //   simplemde.value('');
-  // }
-});
-
-// 打开左侧树型文件
-window.document.addEventListener(EventConstants.CLIENT.TREE_OPEN, (event: any) => {
-  let file = JSON.parse(event.detail).filename;
-  state.currentFile = file;
-  state.isOneFile = true;
-
-  ipcRenderer.send(EventConstants.PHODIT.SAVE_FILE, {
-    isTempFile: state.isCurrentFileTemp,
-    data: simplemde.value(),
-  });
-  ipcRenderer.send(EventConstants.PHODIT.OPEN_FILE, file);
-});
-
-// 返回 Markdown 渲染结果
-window.document.addEventListener(EventConstants.CLIENT.SEND_MARKDOWN, (event: any) => {
-  const data = markdownRender(event.detail, state.currentFile);
-  createEvent(EventConstants.CLIENT.GET_RENDERER_MARKDOWN, data);
-});
-
-// Pandoc 转换
-window.document.addEventListener(EventConstants.CLIENT.SHOW_WORD, (event: any) => {
-  swal({
-    title: "Open File", text: "Are you want to Open File", icon: "info", dangerMode: true,
-    buttons: {
-      cancel: {text: "Cancel", visible: true},
-      confirm: {text: "OK"}
+  // ShowSlides
+  window.document.addEventListener(EventConstants.CLIENT.SHOW_SLIDES, () => {
+    if (!this.state.currentFile) {
+      return;
     }
-  }).then((willDelete: any) => {
-    if (willDelete) {
-      ipcRenderer.send(EventConstants.PHODIT.SHOW_WORD, state.currentFile);
+    ipcRenderer.send(EventConstants.PHODIT.SHOW_SLIDES, {
+      isTempFile: this.state.isCurrentFileTemp,
+      file: this.state.currentFile,
+      data: this.simplemde.value(),
+    });
+  });
+
+  // 隐藏 SIDE
+  window.document.addEventListener(EventConstants.CLIENT.HIDDEN_SIDE, () => {
+    document.getElementById('tree-view').setAttribute('style', "display: none;");
+    document.querySelector(".wechat-button").setAttribute("data-clipboard-target", ".editor-preview-side")
+  });
+
+  // 展示 SIDE
+  window.document.addEventListener(EventConstants.CLIENT.SHOW_SIDE, () => {
+    document.querySelector(".wechat-button").removeAttribute("data-clipboard-target")
+    if (this.state.isPath) {
+      document.getElementById('tree-view').setAttribute('style', "display: block;");
     }
   });
-});
 
-// Pandoc 转换
-window.document.addEventListener(EventConstants.CLIENT.SHOW_PDF, (event: any) => {
-  swal({
-    title: "Open File", text: "Are you want to Open File", icon: "info", dangerMode: true,
-    buttons: {
-      cancel: {text: "Cancel", visible: true},
-      confirm: {text: "OK"}
-    }
-  }).then((willDelete: any) => {
-    if (willDelete) {
-      ipcRenderer.send(EventConstants.PHODIT.SHOW_PDF, state.currentFile);
-    }
+  // 发起获取自动完成请求
+  window.document.addEventListener(EventConstants.CLIENT.GET_SUGGEST, (data: any) => {
+    ipcRenderer.send(EventConstants.PHODIT.GET_SUGGEST, data.detail);
   });
-});
 
-function init() {
-  simplemde.codemirror.on('change', function(cm: any, change: any) {
+  // 打开左侧树型文件
+  window.document.addEventListener(EventConstants.CLIENT.TREE_OPEN, (event: any) => {
+    let file = JSON.parse(event.detail).filename;
+    this.state.currentFile = file;
+    this.state.isOneFile = true;
 
+    ipcRenderer.send(EventConstants.PHODIT.SAVE_FILE, {
+      isTempFile: this.state.isCurrentFileTemp,
+      data: this.simplemde.value(),
+    });
+    ipcRenderer.send(EventConstants.PHODIT.OPEN_FILE, file);
   });
-}
 
-init();
+  // 返回 Markdown 渲染结果
+  window.document.addEventListener(EventConstants.CLIENT.SEND_MARKDOWN, (event: any) => {
+    const data = markdownRender(event.detail, this.state.currentFile);
+    createEvent(EventConstants.CLIENT.GET_RENDERER_MARKDOWN, data);
+  });
+
+  // Pandoc 转换
+  window.document.addEventListener(EventConstants.CLIENT.SHOW_WORD, (event: any) => {
+    swal({
+      title: "Open File", text: "Are you want to Open File", icon: "info", dangerMode: true,
+      buttons: {
+        cancel: {text: "Cancel", visible: true},
+        confirm: {text: "OK"}
+      }
+    }).then((willDelete: any) => {
+      if (willDelete) {
+        ipcRenderer.send(EventConstants.PHODIT.SHOW_WORD, this.state.currentFile);
+      }
+    });
+  });
+
+  // Pandoc 转换
+  window.document.addEventListener(EventConstants.CLIENT.SHOW_PDF, (event: any) => {
+    swal({
+      title: "Open File", text: "Are you want to Open File", icon: "info", dangerMode: true,
+      buttons: {
+        cancel: {text: "Cancel", visible: true},
+        confirm: {text: "OK"}
+      }
+    }).then((willDelete: any) => {
+      if (willDelete) {
+        ipcRenderer.send(EventConstants.PHODIT.SHOW_PDF, this.state.currentFile);
+      }
+    });
+  });
+
+  // 返回获取自动完成请求
+  ipcRenderer.on(EventConstants.PHODIT.SUGGEST_SEND, (event: any, arg: any) => {
+    createEvent(EventConstants.PHODIT.SUGGEST_TO_EDITOR, arg);
+  });
+
+  // 打开文件
+  ipcRenderer.on(EventConstants.PHODIT.OPEN_ONE_FILE, (event: any, arg: IFileOpen) => {
+    this.state.currentFile = arg.file;
+    this.state.isOneFile = true;
+    this.simplemde.codemirror.setOption("mode", getCodeMirrorMode(this.state.currentFile));
+    this.state.isCurrentFileTemp = arg.isTempFile;
+    this.simplemde.value(arg.data);
+    updatePos(arg.file);
+
+    localStorage.setItem('currentFile', arg.file);
+  });
+
+  // 保存文件
+  ipcRenderer.on(EventConstants.CLIENT.SAVE_FILE, () => {
+    ipcRenderer.send(EventConstants.PHODIT.SAVE_FILE, {
+      isTempFile: this.state.isCurrentFileTemp,
+      data: this.simplemde.value(),
+    });
+  });
+
+  // 打开某一目录
+  ipcRenderer.on(EventConstants.PHODIT.OPEN_PATH, (event: any, arg: any) => {
+    this.state.isPath = true;
+    document.getElementById('tree-view').setAttribute('style', "display: block");
+
+    createEvent("phodit.tree.open", {
+      path: arg.path,
+      tree: arg.tree
+    });
+  });
+
+  // 改变临时文件的状态
+  ipcRenderer.on(EventConstants.TEMP_FILE_STATUS, (event: any, arg: any) => {
+    this.state.isCurrentFileTemp = arg.isTempFile;
+  });
+
+  // 加载完成
+  ipcRenderer.on('phodit.lifecycle.load', (event: any, arg: any) => {
+    // if (!this.state.currentFile) {
+    //   this.simplemde.value('');
+    // }
+  });
+};
+
+let client = new ClientUI();
+client.init();
+client.bindEvent();
