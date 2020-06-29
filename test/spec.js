f74234585e28bf8884c5f0a560e92cb14ba792de
Phodal Huang <h@phodal.com>
2020-06-30 07:51:08 +0800

    build: update config for ttravis ci

--- test/spec.js
+++ test/spec.js
@@ -9,12 +9,7 @@ describe('Application launch', function () {
   beforeEach(function () {
     this.app = new Application({
       path: electronPath,
-      args: [path.join(__dirname, '..')],
-      chromeDriverArgs: [
-        '--no-sandbox',
-        '--whitelisted-ips=',
-        '--disable-dev-shm-usage'
-      ],
+      args: [path.join(__dirname, '..')]
     })
     return this.app.start()
   })
