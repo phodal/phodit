84578a80f8a0afa242f1ea87c0d3a4229f82ab9b
Phodal Huang <h@phodal.com>
2020-11-22 21:11:36 +0800

    build: update angular

--- components/interact/src/app/interact-bar.component.ts
+++ components/interact/src/app/interact-bar.component.ts
@@ -1,11 +1,13 @@
 import {Component, EventEmitter, Input, Output, ViewEncapsulation} from '@angular/core';
 
 @Component({
+  // tslint:disable-next-line:component-selector
   selector: 'interact-bar',
   templateUrl: './interact-bar.component.html',
   styleUrls: ['./interact-bar.component.css'],
-  encapsulation: ViewEncapsulation.Native
+  encapsulation: ViewEncapsulation.Emulated
 })
+// tslint:disable-next-line:component-class-suffix
 export class InteractBar {
   @Input() set filename(value: string) {
     this.renameModel.name = value;
