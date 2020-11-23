2da2f13fdc4bad6f5ed972389da4586e386af261
Phodal Huang <h@phodal.com>
2020-11-23 12:35:14 +0800

    feat: add touchbar toggle theme support

--- src/app/touch-bar.ts
+++ src/app/touch-bar.ts
@@ -1,17 +1,31 @@
-const {app, BrowserWindow, TouchBar} = require('electron');
-const {TouchBarLabel, TouchBarButton, TouchBarSpacer} = TouchBar;
+import {BrowserWindow} from "electron";
 
-const theme = new TouchBarButton({
-  label: '🎰 Theme',
-  backgroundColor: '#7851A9',
-  click: () => {
+const {TouchBar} = require('electron');
 
-  }
-});
+import {EventConstants} from '../common/constants/event.constants';
 
-export const touchBar = new TouchBar({
-  items: [
-    theme,
-    new TouchBarSpacer({size: 'large'}),
-  ]
-});
+const {TouchBarButton, TouchBarSpacer} = TouchBar;
+
+export default function buildTouchBar(mainWindow: BrowserWindow) {
+  let dark = false;
+  const theme = new TouchBarButton({
+    label: '🎰 Theme',
+    backgroundColor: '#384452',
+    click: () => {
+      mainWindow.webContents.send(EventConstants.PHODIT.TOGGLE_THEME, {});
+      dark = !dark;
+      if(dark) {
+        theme.backgroundColor = '#384452';
+      } else {
+        theme.backgroundColor = '#fff';
+      }
+    }
+  });
+
+  return new TouchBar({
+    items: [
+      theme,
+      new TouchBarSpacer({size: 'large'}),
+    ]
+  });
+}
